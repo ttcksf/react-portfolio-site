e24c1bf652d6ef95731ae31b65ac5f2fc48e650c
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-03-19 02:06:25 +0900

    react-icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 // 画像をそれぞれimportします。パスに注意してください。
 import CoverImage from '../images/cover-image.jpg';
 import ProfileImage from '../images/profile-image.png';
+import {FaTwitter, FaGithub} from 'react-icons/fa';
 
 export const Header = () => {
   return (
@@ -17,10 +18,14 @@ export const Header = () => {
             <h3 className="title-text">あなたの肩書(ex.Engineer)</h3>
             <ul className="social-icons">
               <li className="icon-link">
-                {/* TODO */}
+                <a href="https://twitter.com">
+                  <FaTwitter color='white' size="2em" />
+                </a>
               </li>
               <li className="icon-link">
-                {/* TODO */}
+                <a href="https://github.com">
+                  <FaGithub color='white' size="2em" />
+                </a>
               </li>
             </ul>
           </div>
